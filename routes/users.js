dc8ca0bf90d36888684dd5e6784842bedc391ac4
edneicristian <ednei.cristian@gmail.com>
2023-03-28 17:36:50 -0300

    Alula70

--- routes/users.js
+++ routes/users.js
@@ -16,12 +16,8 @@ module.exports = (app) => {
 
     });
 
-    app.get('/users/admin', (req, res) => {
-
-        res.statusCode = 200;
-        res.setHeader('Content-text', 'application/json');
-        res.json({
-            users: []
-        });
+    app.post('/users', (req, res) => {
+        
+        res.json(req.body);
     });
 };
